6aa506dc3ea3a0608ed55d5b6cb3e4df81d75ec6
unknown <fengmu@iflytek.com>
2019-11-24 10:34:44 +0800

    编写上传样式和布局

--- src/components/upload.vue
+++ src/components/upload.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-        文件上传在这呢
+    <div class="uplaod-wrap">
+        <div class="select-wrap">
+            <input id="upload" type="file" hidden>
+            <label for="upload"></label>
+        </div>
+        <div class="file-list-wrap">
+            这里是文件上传列表
+        </div>
+        
     </div>
 </template>
 
@@ -11,5 +18,17 @@ export default {
 </script>
 
 <style lang="less" scoped>
+
+.select-wrap {
+    label {
+        display:block;
+        height:100px;
+        width:100px;
+        background:url('../assets/img/add-file-default-icon.png') center no-repeat;
+        &:hover {
+            background:url('../assets/img/add-file-hover-icon.png') center no-repeat;
+        }
+    }
+}
     
 </style>
