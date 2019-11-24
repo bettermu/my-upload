220c11f0f51f68e4511d9dd7ebe41e426db04a89
unknown <fengmu@iflytek.com>
2019-11-24 10:22:11 +0800

    整理结构

--- README.md
+++ README.md
@@ -1,29 +1,3 @@
 # my-upload
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+> 利用空余时间，自己写的一个vue文件上传组件
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <my-upload></my-upload>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myUpload from './components/upload.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    myUpload
   }
 }
 </script>
@@ -21,7 +20,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
